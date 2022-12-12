81bc1a84c6019ea030a29289c6e282a3c97b38e1
stlhorizon <anandgupadhyay03@gmail.com>
2022-12-12 17:49:03 +0530

    Bubble, Merge and Comb sorting
    Bubble, Merge and Comb sorting

---
+++ SortingInSwift.playground/Contents.swift
@@ -0,0 +1,125 @@
+import UIKit
+/***
+ 
+ ***/
+//Simple Bubble Sort
+var greeting = "Hello, playground"
+public var array = [10,34,32,65,17,87,23,98,13,5,3,88,54,67,31,21,76,98,48,28,83,49,28,6,8,2]
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    //a = 10 b = 5
+    a = a + b // 15 (10+5)
+    b = a - b // 10 (15-5)
+    a = a - b // 5 (15-10)
+}
+for i in 0..<array.count {
+  for j in 1..<array.count - i {
+    if array[j] < array[j-1] {
+        //Inbuilt Swap Function for array
+        array.swapAt(j, j-1)
+        //swap Fucntion
+        
+        //Simple Swap in the code
+//      let tmp = array[j-1]
+//      array[j-1] = array[j]
+//      array[j] = tmp
+    }
+  }
+}
+print( "Sorted\(array)")
+
+//Int Stack
+struct IntStack {
+    var items: [Int] = []
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+}
+
+//Generic Stack
+struct Stack<Element> {
+    var items: [Element] = []
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+//============================
+public var array1 = [10,34,32,65,17,87,23,98,13,5,3,88,54,67,31,21,76,98,48,28,83,49,28,6,8,2]
+
+
+//Merge Sort
+func mergeSort<T: Comparable>(_ array: [T]) -> [T] {
+  guard array.count > 1 else { return array }
+  let middleIndex = array.count / 2
+  let leftArray = mergeSort(Array(array[0..<middleIndex]))
+  let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
+  return merge(leftPile: leftArray, rightPile: rightArray)
+}
+
+func merge<T: Comparable>(leftPile: [T], rightPile: [T]) -> [T] {
+  var leftIndex = 0
+  var rightIndex = 0
+  var orderedPile: [T] = []
+  if orderedPile.capacity < leftPile.count + rightPile.count {
+    orderedPile.reserveCapacity(leftPile.count + rightPile.count)
+  }
+
+  while true {
+      guard leftIndex < leftPile.endIndex else {
+          orderedPile.append(contentsOf: rightPile[rightIndex..<rightPile.endIndex])
+          break
+      }
+      guard rightIndex < rightPile.endIndex else {
+          orderedPile.append(contentsOf: leftPile[leftIndex..<leftPile.endIndex])
+          break
+      }
+      
+      if leftPile[leftIndex] < rightPile[rightIndex] {
+          orderedPile.append(leftPile[leftIndex])
+          leftIndex += 1
+      } else {
+          orderedPile.append(rightPile[rightIndex])
+          rightIndex += 1
+      }
+  }
+
+  return orderedPile
+}
+
+mergeSort(array1)
+print("Merge Sort\(array1)")
+
+//====================
+public var array2 = [10,34,32,65,17,87,23,98,13,5,3,88,54,67,31,21,76,98,48,28,83,49,28,6,8,2]
+//Combo Sort
+
+func combSort (input: [Int]) -> [Int] {
+    var copy: [Int] = input
+    var gap = copy.count
+    let shrink = 1.3
+
+    while gap > 1 {
+        gap = (Int)(Double(gap) / shrink)
+        if gap < 1 {
+            gap = 1
+        }
+    
+        var index = 0
+        while !(index + gap >= copy.count) {
+            if copy[index] > copy[index + gap] {
+                copy.swapAt(index, index + gap)
+            }
+            index += 1
+        }
+    }
+    return copy
+}
+
+combSort(input: array2)
+print("Sorted Array:\(array2)")
